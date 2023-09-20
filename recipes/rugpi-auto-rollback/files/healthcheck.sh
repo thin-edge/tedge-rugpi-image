ad859020ed4fdb62fef8e772f6dd7a276b418967
Reuben Miller <reuben.d.miller@gmail.com>
2023-09-20 15:16:24 +1000

    try to wait for broker to be ready before publishing
    Also ignore publishing errors

--- recipes/rugpi-auto-rollback/files/healthcheck.sh
+++ recipes/rugpi-auto-rollback/files/healthcheck.sh
@@ -43,15 +43,15 @@ collect_rugpi() {
     # TODO: Change to te topic once inventory updates are supported
     # c8y payload
     PAYLOAD=$(printf '{"rugpi":{"hot":"%s","default":"%s"}}' "$HOT" "$DEFAULT")
-    tedge mqtt pub "c8y/inventory/managedObjects/update/$DEVICE_ID" "$PAYLOAD"
+    tedge mqtt pub "c8y/inventory/managedObjects/update/$DEVICE_ID" "$PAYLOAD" ||:
 
     # publish to tedge api which is not yet supported
     PAYLOAD=$(printf '{"hot":"%s","default":"%s"}' "$HOT" "$DEFAULT")
-    tedge mqtt pub "$TARGET/data/rugpi" "$PAYLOAD"
+    tedge mqtt pub "$TARGET/data/rugpi" "$PAYLOAD" ||:
 
     # publish event for chronological order
     PAYLOAD=$(printf '{"text":"Partition info. hot=%s, default=%s"}' "$HOT" "$DEFAULT")
-    tedge mqtt pub "$TARGET/e/device_boot" "$PAYLOAD"
+    tedge mqtt pub "$TARGET/e/device_boot" "$PAYLOAD" ||:
 }
 
 collect_os_info() {
@@ -62,19 +62,36 @@ collect_os_info() {
     # Collect Operating System information
     if [ -f /etc/os-release ]; then
         # shellcheck disable=SC1091
-        . /etc/os-release || true
+        . /etc/os-release ||:
 
         # TODO: Change to te topic once inventory updates are supported
         PAYLOAD=$(printf '{"device_OS":{"family":"%s","version":"%s"}}' "$NAME" "${VERSION:-$VERSION_ID}")
-        tedge mqtt pub "c8y/inventory/managedObjects/update/$DEVICE_ID" "$PAYLOAD"
+        tedge mqtt pub "c8y/inventory/managedObjects/update/$DEVICE_ID" "$PAYLOAD" ||:
 
         # publish to tedge api which is not yet supported
         PAYLOAD=$(printf '{"family":"%s","version":"%s"}' "$NAME" "${VERSION:-$VERSION_ID}")
-        tedge mqtt pub --retain "$TARGET/data/device_OS" "$PAYLOAD"
+        tedge mqtt pub --retain "$TARGET/data/device_OS" "$PAYLOAD" ||:
     fi
 }
 
+try_wait_for_broker() {
+    # Try to wait until the broker is ready before publishing data.
+    # Proceed anyway if it still is not ready have N tries
+    RETRIES=10
+    while [ "$RETRIES" -gt 0 ]; do
+        if tedge mqtt pub 'dummy/message' ''; then
+            echo "Broker is ready" >&2
+            return 0
+        fi
+        RETRIES=$((RETRIES - 1))
+        sleep 5
+    done
+
+    echo "Broker is not ready, but continuing anyway" >&2
+}
+
 publish_system_info() {
+    try_wait_for_broker
     collect_rugpi
     collect_os_info
 }
