56d27eb0292fabc1556f06f38c50d609f1cf6225
Reuben Miller <reuben.d.miller@gmail.com>
2023-09-20 14:50:09 +1000

    install collectd and default tedge settings

--- recipes/thin-edge.io/steps/00-install.sh
+++ recipes/thin-edge.io/steps/00-install.sh
@@ -3,6 +3,13 @@
 # install thin-edge.io
 curl -fsSL https://thin-edge.io/install.sh | sh -s
 
+# Install collectd
+apt-get install -y --no-install-recommends collectd-core mosquitto-clients
+cp /etc/tedge/contrib/collectd/collectd.conf /etc/collectd/collectd.conf
+
+# custom tedge configuration
+tedge config set apt.name "(tedge|c8y|python|wget|vim|curl|apt|mosquitto|ssh|sudo).*"
+
 # setup thin-edge.io community repository
 curl -1sLf 'https://dl.cloudsmith.io/public/thinedge/community/setup.deb.sh' | bash
 apt-get update
@@ -14,3 +21,6 @@ systemctl enable NetworkManager || true
 # Enable services by default to have sensible default settings once tedge is configured
 systemctl enable tedge-agent
 systemctl enable tedge-mapper-c8y
+systemctl enable tedge-mapper-collectd
+systemctl enable collectd
+
