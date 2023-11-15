2f9230d070be9ececa8e23fd38b2d99a56e6b93b
Reuben Miller <reuben.d.miller@gmail.com>
2023-11-15 21:22:51 +0100

    refactor thin-edge.io installation script based on the latest version

--- recipes/thin-edge.io/steps/00-install.sh
+++ recipes/thin-edge.io/steps/00-install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -e
 
 # install thin-edge.io
-curl -fsSL https://thin-edge.io/install.sh | sh -s
+curl -fsSL https://thin-edge.io/install.sh | sh -s -- --channel main
 
 # Install collectd
-apt-get install -y --no-install-recommends collectd-core mosquitto-clients
-cp /etc/tedge/contrib/collectd/collectd.conf /etc/collectd/collectd.conf
+apt-get install -y --no-install-recommends \
+    mosquitto-clients \
+    c8y-command-plugin \
+    tedge-collectd-setup
 
 # custom tedge configuration
 tedge config set apt.name "(tedge|c8y|python|wget|vim|curl|apt|mosquitto|ssh|sudo).*"
 
-# setup thin-edge.io community repository
-curl -1sLf 'https://dl.cloudsmith.io/public/thinedge/community/setup.deb.sh' | bash
-apt-get update
-apt-get install -y c8y-command-plugin
 
 # Enable network manager by default
 systemctl enable NetworkManager || true
