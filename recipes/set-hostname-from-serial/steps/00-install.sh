efe569a3b33e2df230bbb31e6cd00d8b91378263
reubenmiller <reuben.d.miller@gmail.com>
2023-11-19 09:43:26 +0100

    remove unused recipe

--- recipes/set-hostname-from-serial/steps/00-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-install -D -m 755 "${RECIPE_DIR}/files/set-hostname.sh" -t /usr/bin/
-install -D -m 644 "${RECIPE_DIR}/files/startup-hostname.service" -t /usr/lib/systemd/system/
-
-systemctl disable startup-hostname.service
