12d4817fcbc4c58e651f486a06964a88dcc864e2
reubenmiller <reuben.d.miller@gmail.com>
2023-11-29 10:30:27 +0100

    fix permissions on health check folders

--- recipes/rugpi-auto-rollback/steps/00-install.sh
+++ recipes/rugpi-auto-rollback/steps/00-install.sh
@@ -2,7 +2,7 @@
 set -eu
 
 mkdir -p /etc/health.d
-chmod 644 /etc/health.d
+chmod 755 /etc/health.d
 
 install -D -m 755 "${RECIPE_DIR}/files/health.d/"* -t /etc/health.d/
 install -D -m 755 "${RECIPE_DIR}/files/healthcheck.sh" -t /usr/bin/
