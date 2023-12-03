d52654128867cc6d7d9201d2fd870eb2d84e2e55
Reuben Miller <reuben.d.miller@gmail.com>
2023-12-03 22:38:45 +0100

    install firmware workflow in image and symlink to persisted folder (#18)
    * install firmware workflow in image and symlink to persisted folder
    
    This allows updating the workflow within the image itself (at least after the reboot).
    
    * fix firmware version install path

--- recipes/tedge-firmware-update/steps/00-install.sh
+++ recipes/tedge-firmware-update/steps/00-install.sh
@@ -2,6 +2,9 @@
 set -eu
 install -D -m 644 "${RECIPE_DIR}/files/tedge-firmware" -t /etc/sudoers.d/
 install -D -m 644 "${RECIPE_DIR}/files/system.toml" -t /etc/tedge/
-install -D -m 644 "${RECIPE_DIR}/files/firmware_update.toml" -t /etc/tedge/operations/
+install -D -m 644 "${RECIPE_DIR}/files/firware_update.rugpi.toml" -t /usr/share/tedge-workflows/
 install -D -m 755 "${RECIPE_DIR}/files/rugpi_workflow.sh" -t /usr/bin/
-install -D -m 755 "${RECIPE_DIR}/files/firmware-version" -t /usr/share/tedge-inventory/scripts.d/80_c8y_Firmware
+install -D -m 755 "${RECIPE_DIR}/files/firmware-version" /usr/share/tedge-inventory/scripts.d/80_c8y_Firmware
+
+# Use symlink so that the workflow file can be updated within the image
+ln -s /usr/share/tedge-workflows/firware_update.rugpi.toml /etc/tedge/operations/firmware_update.toml
