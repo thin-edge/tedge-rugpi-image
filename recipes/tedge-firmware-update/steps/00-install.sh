c5d93835da1d09cbb756b9935849fd362d320998
reubenmiller <reuben.d.miller@gmail.com>
2023-12-03 22:08:19 +0100

    add firmware info inventory script

--- recipes/tedge-firmware-update/steps/00-install.sh
+++ recipes/tedge-firmware-update/steps/00-install.sh
@@ -4,3 +4,4 @@ install -D -m 644 "${RECIPE_DIR}/files/tedge-firmware" -t /etc/sudoers.d/
 install -D -m 644 "${RECIPE_DIR}/files/system.toml" -t /etc/tedge/
 install -D -m 644 "${RECIPE_DIR}/files/firmware_update.toml" -t /etc/tedge/operations/
 install -D -m 755 "${RECIPE_DIR}/files/rugpi_workflow.sh" -t /usr/bin/
+install -D -m 755 "${RECIPE_DIR}/files/firmware-version" -t /usr/share/tedge-inventory/scripts.d/80_c8y_Firmware
