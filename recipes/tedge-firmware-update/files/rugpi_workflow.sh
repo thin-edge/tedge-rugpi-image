b44022b70577abfbc1d07cd1f55e34a52227b8be
Reuben Miller <reuben.d.miller@gmail.com>
2023-12-14 10:37:54 +0100

    update workflow syntax to support latest thin-edge.io version

--- recipes/tedge-firmware-update/files/rugpi_workflow.sh
+++ recipes/tedge-firmware-update/files/rugpi_workflow.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 set -e
-ON_SUCCESS="successful"
-ON_ERROR="failed"
-ON_RESTART="restart"
 FIRMWARE_NAME=
 FIRMWARE_VERSION=
 FIRMWARE_URL=
 FIRMWARE_META_FILE=/etc/tedge/.firmware
 MANUAL_DOWNLOAD=0
 
+# Exit codes
+OK=0
+FAILED=1
+REQUEST_RESTART=4
+
 # Use temp directory so that the file can't accidentally persist across partitions
 # thus always booting into the spare partition
 REBOOT_SPARE_REQUEST=/tmp/.reboot_spare
 
-SUDO="sudo"
+# Detect if sudo should be used or not. It will be used if it is found
+SUDO=""
+if command -V sudo >/dev/null 2>&1; then
+    SUDO="sudo"
+fi
 
 _WORKDIR=$(pwd)
 
@@ -22,18 +28,17 @@ cd /tmp || cd /
 
 HOT=$(rugpi-ctrl system info | grep Hot | cut -d: -f2 | tr '[:lower:]' '[:upper:]' | xargs)
 DEFAULT=$(rugpi-ctrl system info | grep Default | cut -d: -f2 | tr '[:lower:]' '[:upper:]' | xargs)
+SPARE=$(rugpi-ctrl system info | grep Spare | cut -d: -f2 | tr '[:lower:]' '[:upper:]' | xargs)
 
 ACTION="$1"
 shift
-
 
 log() {
     msg="$(date +%Y-%m-%dT%H:%M:%S) [current=$ACTION] $*"
     echo "$msg" >&2
 
     # publish to pub for better resolution
-    current_partition=$(get_current_partition)
-    tedge mqtt pub -q 2 te/device/main///e/firmware_update "{\"text\":\"Firmware Workflow: [$ACTION] $*\",\"state\":\"$ACTION\",\"partition\":\"$current_partition\"}"
+    tedge mqtt pub -q 2 te/device/main///e/firmware_update "{\"text\":\"Firmware Workflow: [$ACTION] $*\",\"state\":\"$ACTION\",\"partition\":\"$HOT\"}"
     sleep 1
 }
 
@@ -43,32 +48,17 @@ local_log() {
     echo "$msg" >&2
 }
 
-next_state_with_context() {
+update_state() {
     echo ":::begin-tedge:::"
     echo "$1"
     echo ":::end-tedge:::"
     sleep 1
 }
 
-next_state() {
-    status="$1"
-    reason=
-
-    if [ $# -gt 1 ]; then
-        reason="$2"
-    fi
-
-    message=
-    if [ -n "$reason" ]; then
-        log "Moving to next State: $status. reason=$reason"
-        message=$(printf '{"status":"%s","reason":"%s"}' "$status" "$reason")
-    else
-        log "Moving to next State: $status"
-        message=$(printf '{"status":"%s"}' "$status")
-    fi
-
-    next_state_with_context "$message"
-    sleep 1
+set_reason() {
+    reason="$1"
+    message=$(printf '{"reason":"%s"}' "$reason")
+    update_state "$message"
 }
 
 #
@@ -86,18 +76,6 @@ while [ $# -gt 0 ]; do
             ;;
         --url)
             FIRMWARE_URL="$2"
-            shift
-            ;;
-        --on-success)
-            ON_SUCCESS="$2"
-            shift
-            ;;
-        --on-error)
-            ON_ERROR="$2"
-            shift
-            ;;
-        --on-restart)
-            ON_RESTART="$2"
             shift
             ;;
     esac
@@ -142,27 +120,16 @@ wait_for_network() {
     return 1
 }
 
-get_current_partition() {
-    current=$(rugpi-ctrl system info | grep Hot | cut -d: -f2 | xargs | tr '[:lower:]' '[:upper:]')
-    echo "$current"
-}
-
-get_next_partition() {
-    [ "$1" = "A" ] && echo "B" || echo "A"
-}
-
 executing() {
-    current_partition=$(get_current_partition)
-    next_partition=$(get_next_partition "$current_partition")
-    echo "---------------------------------------------------------------------------"
-    echo "Firmware update: $current_partition -> $next_partition"
-    echo "---------------------------------------------------------------------------"
-    log "Starting firmware update. Current partition is $(get_current_partition), so update will be applied to $next_partition"
+    if [ "$HOT" != "$DEFAULT" ]; then
+        set_reason "Refusing to install update as the current (hot) partition is not the default partition. This indicates that you may be in the middle of an update. Please reboot to switch to the default partition"
+        exit "$FAILED"
+    fi
+    log "Starting firmware update. Current partition is $HOT, so update will be applied to $SPARE"
 }
 
 download() {
-    status="$1"
-    url="$2"
+    url="$1"
 
     #
     # Change url to a local url using the c8y proxy
@@ -184,17 +151,16 @@ download() {
             ;;
         *)
             # Assume url is actually a file and just go to the next state
-            next_state_with_context "$(printf '{"status":"%s","url":"%s"}\n' "$status" "$url")"
-            return 0
+            update_state "$(printf '{"url":"%s"}\n' "$url")"
+            return "$OK"
             ;;
     esac
 
-    TEDGE_DATA=$(tedge config get data.path)
-
     if [ "$MANUAL_DOWNLOAD" = 1 ]; then
+        TEDGE_DATA=$(tedge config get data.path)
 
         # Removing any older files to ensure space for next file to download
-        # Note: busy box does not support -delete
+        # Note: busy box does not support the -delete flag
         find "$TEDGE_DATA" -name "*.firmware" -exec rm {} \;
 
         last_part=$(echo "$partial_path" | rev | cut -d/ -f1 | rev)
@@ -202,10 +168,10 @@ download() {
         log "Manually downloading artifact from $tedge_url and saving to $local_file"
         wget -c -O "$local_file" "$tedge_url" >&2
         log "Downloaded file from: $tedge_url"
-        next_state_with_context "$(printf '{"status":"%s","url":"%s"}\n' "$status" "$local_file")"
+        update_state "$(printf '{"url":"%s"}\n' "$local_file")"
     else
         log "Converted to local url: $url => $tedge_url"
-        next_state_with_context "$(printf '{"status":"%s","url":"%s"}\n' "$status" "$tedge_url")"
+        update_state "$(printf '{"url":"%s"}\n' "$tedge_url")"
     fi
 }
 
@@ -214,13 +180,13 @@ install() {
     set +e
     case "$url" in
         http://*.img|https://*.img)
-            log "Executing: wget -c -q -t 0 -O - '$url' | $SUDO rugpi-ctrl update install --stream --no-reboot -"
+            log "Downloading and streaming uncompressed image to rugpi"
             wget -c -q -t 0 -O - "$url" | $SUDO rugpi-ctrl update install --stream --no-reboot -
             ;;
 
         # Assume a xz compressed file        
         http://*|https://*)
-            log "Executing: wget -c -q -t 0 -O - '$url' | xz -d | $SUDO rugpi-ctrl update install --stream --no-reboot -"
+            log "Downloading and streaming xz compressed image to rugpi"
             wget -c -q -t 0 -O - "$url" | xz -d | $SUDO rugpi-ctrl update install --stream --no-reboot -
             ;;
 
@@ -232,12 +198,12 @@ install() {
             case "$mime_type" in
                 application/x-xz)
                     # Decode the file and stream it into rugpi (decompressing on the fly)
-                    log "Executing: xz -d -T0 -c '$url' | $SUDO rugpi-ctrl update install --stream --no-reboot -"
+                    log "Installing local xz compressed image to rugpi"
                     xz --decompress --stdout -T0 "$url" | $SUDO rugpi-ctrl update install --stream --no-reboot -
                     ;;
                 *)
                     # Uncompressed file
-                    log "Executing: rugpi-ctrl update install --no-reboot '$url'"
+                    log "Installing local uncompressed image to rugpi"
                     $SUDO rugpi-ctrl update install --no-reboot "$url"
                     ;;
             esac
@@ -249,37 +215,33 @@ install() {
     case "$EXIT_CODE" in
         0)
             log "OK, RESTART required"
-            next_state "$ON_RESTART"
             ;;
         *)
             log "ERROR. Unexpected return code. code=$EXIT_CODE"
-            next_state "$ON_ERROR" "ERROR. Unexpected return code. code=$EXIT_CODE"
             ;;
     esac
 
     # Create mark file which is used by the restart state to reboot into the spare partition
     touch "$REBOOT_SPARE_REQUEST"
+    exit "$EXIT_CODE"
 }
 
 restart() {
     # NOTE: This function should not be called in the script directly but rather via the system.toml
-    current_partition=$(get_current_partition)
-    next_partition=$(get_next_partition "$current_partition")
-
     if [ -f "$REBOOT_SPARE_REQUEST" ]; then
         rm -f "$REBOOT_SPARE_REQUEST"
 
-        message=$(printf '{"text":"Rebooting into spare partition (%s -> %s)","partition":"%s"}' "$current_partition" "$next_partition" "$current_partition")
+        message=$(printf '{"text":"Rebooting into spare partition (%s -> %s)","partition":"%s"}' "$HOT" "$SPARE" "$HOT")
         tedge mqtt pub -q 1 "te/device/main///e/reboot_spare" "$message" ||:
         sleep 5
         $SUDO rugpi-ctrl system reboot --spare
     else
-        message=$(printf '{"text":"Rebooting into default partition (%s -> %s)","partition":"%s"}' "$current_partition" "$DEFAULT" "$current_partition")
+        message=$(printf '{"text":"Rebooting into default partition (%s -> %s)","partition":"%s"}' "$HOT" "$DEFAULT" "$HOT")
         tedge mqtt pub -q 1 "te/device/main///e/reboot_default" "$message" ||:
         sleep 5
         $SUDO rugpi-ctrl system reboot
     fi
-    exit 0
+    exit "$OK"
 }
 
 verify() {
@@ -287,16 +249,16 @@ verify() {
 
     if [ "$HOT" = "$DEFAULT" ]; then
         # Don't both to reboot if no partition swap occurred because we are already in the ok partition
-        next_state "$ON_ERROR" "Partition swap did not occur. Reasons could be, corrupt/non-bootable image, someone did a manual rollback or the machine was restarted manually before the health check was run"
-        return
+        set_reason "Partition swap did not occur. Reasons could be, corrupt/non-bootable image, someone did a manual rollback or the machine was restarted manually before the health check was run"
+        exit "$FAILED"
     fi
 
     # Allow users to also call addition logic by adding their scripts to the /etc/health.d/ directory
     if /usr/bin/healthcheck.sh; then
-        next_state "$ON_SUCCESS"
+        exit "$OK"
     else
-        log "Health check failed on new partition"
-        next_state "$ON_RESTART"
+        set_reason "Health check failed on new partition"
+        exit "$REQUEST_RESTART"
     fi
 }
 
@@ -309,51 +271,40 @@ commit() {
 
     case "$EXIT_CODE" in
         0)
-            log "Commit successful. New default partition is $(get_current_partition)"
-
+            log "Commit successful. New default partition is $DEFAULT"
             # Save firmware meta information to file (for reading on startup during normal operation)
             local_log "Saving firmware info to $FIRMWARE_META_FILE"
             printf 'FIRMWARE_NAME=%s\nFIRMWARE_VERSION=%s\nFIRMWARE_URL=%s\n' "$FIRMWARE_NAME" "$FIRMWARE_VERSION" "$FIRMWARE_URL" > "$FIRMWARE_META_FILE"
-
-            next_state "$ON_SUCCESS"
             ;;
         *)
             log "rugpi-ctrl returned code: $EXIT_CODE. Rolling back to previous partition"
-            next_state "$ON_RESTART"
             ;;
     esac
+    exit "$EXIT_CODE"
 }
 
 case "$ACTION" in
-    executing)
-        executing
-        next_state "$ON_SUCCESS"
-        ;;
-    download) download "$ON_SUCCESS" "$FIRMWARE_URL"; ;;
+    executing) executing; ;;
+    download) download "$FIRMWARE_URL"; ;;
     install) install "$FIRMWARE_URL"; ;;
     verify) verify; ;;
     commit) commit; ;;
     restart) restart; ;;
     restarted)
 	    wait_for_network ||:
-        log "Device has been restarted...continuing workflow. partition=$(get_current_partition)"
-        next_state "$ON_SUCCESS"
+        log "Device has been restarted...continuing workflow. partition=$HOT, default=$DEFAULT"
         ;;
     rollback_successful)
-        next_state "$ON_SUCCESS" "Firmware update failed, but the rollback was successful. partition=$(get_current_partition)"
+        log "Firmware update failed, but the rollback was successful. partition=$HOT, default=$DEFAULT"
         ;;
-    failed_restart)
-        # There is no success/failed action here, we always transition to the next state
-        # Only an error reason is added
-        next_state "$ON_SUCCESS" "Device failed to restart"
-        ;;
+    failed_restart) ;;
     *)
         log "Unknown command. This script only accepts: download, install, commit, rollback, rollback_successful, failed_restart"
-        exit 1
+        exit "$FAILED"
         ;;
 esac
 
 # switch back to original directory
 cd "$_WORKDIR" ||:
 
-exit 0
+exit "$OK"
