93ca37fb1ae3a89f8ef2530f4be48f59b0cd1a1e
Reuben Miller <reuben.d.miller@gmail.com>
2024-01-11 21:24:45 +0100

    fix(build): auto detection when to use when compressing the image using xz

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -2,10 +2,22 @@
 set -e
 
 SUDO=
-if [ -n "$CI" ]; then
-    # use sudo when running in the CI otherwise the following error occurs
-    # regardless of the owner of the file and folder.
-    # xz: Cannot set the file group: Operation not permitted
+
+requires_sudo() {
+    if stat --help >/dev/null 2>&1; then
+        IMAGE_OWNER=$(stat -c "%u" "$1")
+    else
+        # bsd variant
+        IMAGE_OWNER=$(stat -f "%u" "$1")
+    fi
+    [ "$IMAGE_OWNER" = 0 ]
+}
+
+# On some systems docker creates the image as the root user which
+# requires running xz with sudo otherwise the following error occurs:
+# xz: Cannot set the file group: Operation not permitted
+if requires_sudo "$1"; then
+    echo "Image is owned by root, so compressing using sudo" >&2
     SUDO=sudo
 fi
 
