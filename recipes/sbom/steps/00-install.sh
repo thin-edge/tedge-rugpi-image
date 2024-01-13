cc0e1ac55a1a2c874c1a5a7346bfea314fd61ded
Reuben Miller <reuben.d.miller@gmail.com>
2024-01-13 10:29:10 +0100

    feat!: use rugpi repositories and layers feature

--- recipes/sbom/steps/00-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Creating Software Bill Of Materials"
-if [ -n "$RECIPE_DIR" ]; then
-    dpkg --list > "$RECIPE_DIR/debian-packages.list"
-else
-    dpkg --list
-fi
--- recipes/tedge-bootstrap/steps/00-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-install -D -m 644 "${RECIPE_DIR}/files/hostname.toml" -t /etc/rugpi/state/
-install -D -m 755 "${RECIPE_DIR}/files/tedge-identity" -t /usr/bin/
-install -D -m 755 "${RECIPE_DIR}/files/tedge-bootstrap" -t /usr/bin/
-
-install -D -m 644 "${RECIPE_DIR}/files/tedge-bootstrap.service" -t /usr/lib/systemd/system/
-systemctl enable tedge-bootstrap.service
